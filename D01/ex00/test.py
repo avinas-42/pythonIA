bb81bb5044cb31cd7dfc8a6cf0c795369527825a
Arnaud VINAS <avinas@e3r4p21.clusters.42paris.fr>
2021-11-09 16:09:10 +0100

    ex00

--- D01/ex00/test.py
+++ D01/ex00/test.py
@@ -1,4 +1,6 @@
 from recipe import Recipe
+from book import Book
+
 
 def checkInt(text):
     try:
@@ -7,12 +9,29 @@ def checkInt(text):
     except ValueError:
         return False
 
+
 def get_int():
     choice = input(">>")
     if checkInt(choice):
         return int(choice)
     else:
         return -1
+
+
+def fill_book(book):
+    sandwich = Recipe("sandwich", 4, 5,
+                      ["pain", "beur", "cornichon"],
+                      "un sandwich quoi...", "lunch")
+    sandwich2 = Recipe("sandwich2", 2, 4,
+                       ["pain", "beur", "cornichon"],
+                       "un sandwich quoi...", "lunch")
+    sandwich3 = Recipe("sandwich3", 3, 1,
+                       ["pain", "beur"],
+                       "un sandwich quoi...", "dessert")
+    book.add_recipe(sandwich)
+    book.add_recipe(sandwich2)
+    book.add_recipe(sandwich3)
+
 
 def input_a_recipe():
     recipe = input('name of your recipe >> ')
@@ -37,12 +56,20 @@ def input_a_recipe():
         time = get_int()
         if time < 0:
             print('wrong input')
-            
+
     return Recipe(recipe, cooking_lvl, time, ingredients, description, meal)
 
+
 def main():
-    one = input_a_recipe()
-    print(one)
+    book = Book("cookbook")
+    fill_book(book)
+    recipes_list = book.get_recipes_by_types("lunch")
+    for recipe in recipes_list:
+        print(recipe)
+    recipe = book.get_recipe_by_name("sandwich3")
+    book.add_recipe(input_a_recipe())
+    print(book)
+
 
 if __name__ == "__main__":
     main()
